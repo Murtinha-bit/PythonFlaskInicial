3c82e9bbe07d301bb17f366bc1bc9d90b4108356
Murtinha-bit <gabrielmurta7@gmail.com>
2021-09-27 23:37:17 -0300

    Adicionando os arquivos

---
+++ main.py
@@ -0,0 +1,135 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+
+from datetime import date
+
+import sqlite3
+from sqlite3 import Error
+
+
+#######################################################
+# Instancia da Aplicacao Flask
+app = Flask(__name__)
+
+#######################################################
+# 1. Cadastrar produtos
+
+@app.route('/', methods =['GET'])
+def home():
+    return('Pagina Inicial')
+    
+@app.route('/produtos/cadastrar', methods=['GET', 'POST'])
+def cadastrar():
+
+    if request.method == 'POST':
+
+        descricao = request.form['descricao']
+        precocompra = request.form['precocompra']
+        precovenda = request.form['precovenda']
+        datacriacao = date.today()
+
+        mensagem = 'Erro - nao cadastrado'
+
+        if descricao and precocompra and precovenda and datacriacao:
+            registro = (descricao, precocompra, precovenda, datacriacao)
+
+            try:
+                conn = sqlite3.connect('database/db-produtos.db')
+
+                sql = ''' INSERT INTO produtos(descricao, precocompra, precovenda, datacriacao) VALUES(?,?,?,?) '''
+
+                cur = conn.cursor()
+
+                cur.execute(sql, registro)
+
+                conn.commit()
+
+                mensagem = 'Sucesso - cadastrado'
+
+            except Error as e:
+                print(e)
+            finally:
+                conn.close()
+
+    return render_template('cadastrar.html')
+
+@app.route('/produtos/excluir', methods=['GET', 'POST'])
+def excluir():
+    if request.method== 'POST':
+        codigoproduto = request.form['codproduto']
+        print(codigoproduto)
+        if codigoproduto :
+            try:
+                conn = sqlite3.connect('database/db-produtos.db')
+                cur = conn.cursor()
+                sql = '''DELETE FROM produtos WHERE codproduto = ?'''
+                id=codigoproduto
+                cur.execute(sql, (id,))
+                conn.commit()
+                mensagem = 'Sucesso - apagado'
+            except Error as e:
+                print(e)
+            finally:
+                conn.close()
+    return render_template('excluir.html')
+    
+@app.route('/produtos/editar', methods=['GET', 'POST'])
+def editar():
+    if request.method== 'POST':
+        codigoproduto = request.form['codproduto']
+        descricao = request.form['descricao']
+        precocompra = request.form['precocompra']
+        precovenda = request.form['precovenda']
+        if descricao and precocompra and precovenda and codigoproduto:
+            registro = (descricao, precocompra, precovenda, codigoproduto )
+            try:
+                conn = sqlite3.connect('database/db-produtos.db')
+                sql = ''' UPDATE produtos SET descricao = ?, precocompra = ?, precovenda = ? WHERE codproduto = ?'''
+                cur = conn.cursor()
+
+                cur.execute(sql, registro)
+
+                conn.commit()
+
+                mensagem = 'Sucesso - Editado'
+
+                
+            except Error as e:
+                print(e)
+            finally:
+                conn.close()
+    return render_template('editar.html')
+
+
+
+
+
+
+
+#######################################################
+# 2. Listar produtos
+@app.route('/produtos/listar', methods=['GET'])
+def listar():
+    try:
+        conn = sqlite3.connect('database/db-produtos.db')
+        sql = '''SELECT * FROM produtos'''
+        cur = conn.cursor()
+        cur.execute(sql)
+        registros = cur.fetchall()
+        return render_template('listar.html', regs=registros)
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+
+#######################################################
+# Rota de Erro
+@app.errorhandler(404)
+def pagina_nao_encontrada(e):
+    return render_template('404.html'), 404
+
+#######################################################
+
+app.run(host='0.0.0.0', port=80)
